ad3f78df2a2d9311bd0268e31b478231d1ddcc6c
romeollangozi <romeollangozi123@gmail.com>
2023-11-30 10:42:01 +0100

    s

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- SQL DATABASE CREATE AND USER
 CREATE DATABASE IF NOT EXISTS 'hbnb_dev_db';
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd' IF NOT EXISTS;
 GRANT ALL PRIVILEGES ON  'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 
