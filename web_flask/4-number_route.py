76cfd9b0cc782b10dd88e197df40e5fb9d33ca7a
romeollangozi <romeollangozi123@gmail.com>
2023-12-11 12:21:25 +0100

    s

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Module to initiate a flask app
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Index route
+    """
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    hbnb route
+    """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    Dynamic routing
+    """
+    text = text.replace("_", " ")
+    return "C " + text
+
+
+@app.route('/python/')
+@app.route("/python/<text>", strict_slashes=False)
+def python(text='is cool'):
+    """
+    Dynamic routing
+    """
+    if text:
+        return "Python " + text.replace("_", " ")
+    return "Python " + text
+
+
+@app.route('/number/<int:number>')
+def number(number):
+    """
+    Dynamic routing
+    """
+    return str(number) + " is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
