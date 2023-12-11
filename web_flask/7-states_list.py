3563c02693e7ebfea2bc02ab3610f6eece63504d
romeollangozi <romeollangozi123@gmail.com>
2023-12-12 00:40:43 +0100

    s

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,34 @@
+#/usr/bin/python3
+"""
+Creating an app
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Simple function
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    dynamic routing
+    """
+    all_states = [{'id': value.to_dict()['id'],
+                   'state': value.to_dict()['name']}
+                  for value in storage.all(State).values()]
+    all_states = sorted(all_states, key=lambda state: state['state'])
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
