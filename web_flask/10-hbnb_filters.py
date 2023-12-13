7790c33aa2a0a44c1401a40fc68cd5d6a1e9a1d3
romeollangozi <romeollangozi123@gmail.com>
2023-12-13 18:55:53 +0100

    s

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Creating an app
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """
+    Simple function
+    """
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """
+    dynamic routing
+    """
+    data = [value for value in storage.all(State).values()]
+    amenities = [amenity.to_dict()['name'] for amenity in
+                 storage.all(Amenity).values()]
+    return render_template('10-hbnb_filters.html',
+                           data=data, amenities=amenities)
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
